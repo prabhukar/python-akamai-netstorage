d3ae36a6dc8bf02a97fc3b3538d22cc338ac8651
prabhukar <gspl_prabhushankar@yahoo.com>
2015-04-15 14:35:58 -0400

    proxy details

--- netstorage/constants.py
+++ netstorage/constants.py
@@ -1,3 +1,19 @@
+
+## Proxies
+fiddlerproxy = { # for capturing data going via Fiddler
+                "http": "http://localhost:8888",
+                "https": "http://localhost:8888",
+            }
+proxy = { # Internet Facing proxies
+                "http": "https://proxy.jpmchase.net:8443",
+                "https": "https://proxy.jpmchase.net:8443",
+            }
+proxydirect = { # direct proxy for connecting directly
+                "http": "",
+                "https": "",
+            }
+            
+            
 AKAMAI_HOST_POSTFIX = '-nsu.akamaihd.net'
 AKAMAI_AUTH_VERSION = 5
 AKAMAI_PROTOCOL_VERSION = 1
